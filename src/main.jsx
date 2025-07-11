15be018eec2e7cb6a5e3a0bb16e65699dc3df0bc
Lautaro <lauljfa05@gmail.com>
2025-07-11 15:21:35 -0300

    Cambie el enrutado de los links a ver si ahora funciona

--- src/main.jsx
+++ src/main.jsx
@@ -1,51 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ReproductorCaminos from './components/ReproductorCaminos.jsx'
-import ReproductorLove from './components/ReproductorLove.jsx'
-import ReproductorDocumental from './components/ReproductorDocumental.jsx'
-import Nosotros from './components/Nosotros.jsx'
-import Alquiler from './components/Alquiler.jsx'
-import Contacto from './components/Contacto.jsx'
-import Proyectos from './components/Proyectos.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-  },
-  {
-        path:'ReproductorCaminos',
-        element: <ReproductorCaminos/>
-  },
-  {
-        path:'ReproductorLove',
-        element: <ReproductorLove/>
-  },
-  {
-        path:'ReproductorDocumental',
-        element: <ReproductorDocumental/>
-  },
-  {
-        path:'Nosotros',
-        element: <Nosotros/>
-  },
-  {
-        path:'Alquiler',
-        element: <Alquiler/>
-  },
-  {
-      path:'Contacto',
-      element: <Contacto/>
-  },
-  {
-      path:'Proyectos',
-      element: <Proyectos/>
-  },
-]);
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+import ReproductorCaminos from './components/ReproductorCaminos.jsx';
+import ReproductorLove from './components/ReproductorLove.jsx';
+import ReproductorDocumental from './components/ReproductorDocumental.jsx';
+import Nosotros from './components/Nosotros.jsx';
+import Alquiler from './components/Alquiler.jsx';
+import Contacto from './components/Contacto.jsx';
+import Proyectos from './components/Proyectos.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}/>
-)
+  <StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ReproductorCaminos" element={<ReproductorCaminos />} />
+        <Route path="/ReproductorLove" element={<ReproductorLove />} />
+        <Route path="/ReproductorDocumental" element={<ReproductorDocumental />} />
+        <Route path="/Nosotros" element={<Nosotros />} />
+        <Route path="/Alquiler" element={<Alquiler />} />
+        <Route path="/Contacto" element={<Contacto />} />
+        <Route path="/Proyectos" element={<Proyectos />} />
+        {/* Ruta comodín por si escriben algo inexistente */}
+        <Route path="*" element={<App />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
