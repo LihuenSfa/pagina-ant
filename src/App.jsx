c80e053a2a4afd9af4f9577233b62a79254eaad0
Lautaro <lauljfa05@gmail.com>
2025-07-11 14:10:16 -0300

    actualice la app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ import Bridge from "./components/Bridge";
 import YTC1 from "./components/YTC1";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const pathFrom404 = window.location.search.slice(1);
+    if (pathFrom404) {
+      navigate(pathFrom404);
+    }
+  }, []);
 
   return (
     <main>
