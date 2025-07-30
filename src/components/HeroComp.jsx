f4ca6dfe6b5bb243ad40e54abacc1d1b1ecefd5c
Sebastian Flores <lihuensfa@gmail.com>
2025-07-29 22:52:38 -0300

    Primer commit responsivo, cambie el heroComp el tamaño del texto se reduce dependiendo de la pantalla

--- src/components/HeroComp.jsx
+++ src/components/HeroComp.jsx
@@ -25,28 +25,30 @@ const HeroComp = () => {
     }
 
     return (
-        <div className='relative h-screen w-full overflow-hidden'>
-            <video src={currentVideo} className='absolute inset-0 h-full w-full object-cover pointer-events-none'autoPlay muted loop /> 
+        <div className='relative h-screen w-full overflow-hidden bg-black'>
+            <video src={currentVideo} className='opacity-50 absolute inset-0 h-full w-full object-cover pointer-events-none'autoPlay muted loop /> 
 
             {/* Contenido superpuesto */}
-            <div className="relative z-10 flex h-full w-full flex-col items-start justify-center bg-black bg-opacity-50 px-20 pt-24 pb-8 gap-10">
-                <h1 className="font-ducktape text-7xl font-bold text-yellow-10 md:text-7xl self-end mt-16 mb-[14rem]">
-                    Yellow Tapes
-                </h1>
-                <div className="flex flex-col items-start gap-4 font-helvetica text-7xl text-white">
-                    <button onMouseEnter={() => handleHover("heroVid3")} className="flex gap-2 transition hover:text-gray" >
-                        Graffiti Tapes - Lima  
-                        <p className='text-sm'>2025</p>
-                    </button>
-                    
-                    <button onMouseEnter={() => handleHover("heroVid2")} className="flex gap-2 transition hover:text-gray" >
-                        The I love you bridge  
-                        <p className='text-sm'>2021</p>
-                    </button>
-                    <button onMouseEnter={() => handleHover("heroVid1")} className="flex gap-2 transition hover:text-gray" >
-                        Todos mis caminos son la destrucción  
-                        <p className='text-sm'>2024</p>
-                    </button>
+            <div className="relative z-10 h-screen w-full flex flex-col px-16 pt-16 pb-4">
+                <div className='flex flex-col justify-around grow'>
+                    <h1 className="font-ducktape 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl text-3xl font-bold text-yellow-10 text-end">
+                        Yellow Tapes
+                    </h1>
+                    <div className="flex flex-col items-start gap-4 font-helvetica 2xl:text-7xl xl:text-6xl lg:text-5xl md:text-4xl text-3xl text-white">
+                        <button onMouseEnter={() => handleHover("heroVid3")} className="flex gap-2 transition hover:text-gray" >
+                            Graffiti Tapes - Lima  
+                            <p className='text-sm'>2025</p>
+                        </button>
+                        
+                        <button onMouseEnter={() => handleHover("heroVid2")} className="flex gap-2 transition hover:text-gray" >
+                            The I love you bridge  
+                            <p className='text-sm'>2021</p>
+                        </button>
+                        <button onMouseEnter={() => handleHover("heroVid1")} className="flex gap-2 transition hover:text-gray" >
+                            Todos mis caminos son la destrucción  
+                            <p className='text-sm'>2024</p>
+                        </button>
+                    </div>
                 </div>
                 <button onClick={scrollDown} className='self-center rounded-full border-2 border-white px-[2rem] py-[0.5rem] hover:text-gray hover:border-gray pulse'>
                     <FontAwesomeIcon icon={faArrowDown}/>
